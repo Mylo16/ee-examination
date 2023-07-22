8619a67dc4fc4931fe45d92fc962d5578bc2b89c
Antwi Eric <ANTWI@DESKTOP-92MFH60.(none)>
2023-07-22 09:34:04 +0000

    finish

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import React from 'react';
-// import ProgressBar from './progressBar';
+import ProgressBar from './progressBar';
 import correct from '../assets/correct.png';
 import wrong from '../assets/wrong.png';
 
@@ -13,12 +13,12 @@ export default function Results() {
       <div className="results-container">
         <h1 className="results-head">Results:</h1>
         <h2>{resultsData.score}/{resultsData.resultsQuestion.length}</h2>
-        {/* <ProgressBar percentage={percentMark}/> */}
+        <ProgressBar percentage={percentMark}/>
         <h2>Revision</h2>
         <div className="revision-container">
           {
             resultsData.resultsQuestion.map((question) => (
-              <div className="revision-question" key={crypto.randomUUID()}>
+              <div className="revision-question" key={question.question}>
                 <div className="mark"><img src={question.correct ? correct : wrong} alt="mark" /></div>
                 <p>{question.question}</p>
                 {question.quesImage ==="" ? <p /> : <img className="ques-img" src={question.quesImage} alt="question image" />}
